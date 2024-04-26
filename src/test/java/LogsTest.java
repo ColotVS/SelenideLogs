7d64540cc819f78772cd44674288d95fb8e8778b
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-26 13:07:38 +0700

    Переменная ожидания

--- src/test/java/LogsTest.java
+++ src/test/java/LogsTest.java
@@ -1,13 +1,24 @@
 import com.codeborne.selenide.Configuration;
+import org.awaitility.Awaitility;
+import org.awaitility.core.ConditionFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 
 public class LogsTest {
+    private final Supplier<ConditionFactory> WAITER = () -> Awaitility.given()
+            .ignoreExceptions()
+            .pollInterval(3, TimeUnit.SECONDS)
+            .await()
+            .dontCatchUncaughtExceptions()
+            .atMost(10, TimeUnit.SECONDS);
+
     @BeforeEach
     public void setUp() {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
