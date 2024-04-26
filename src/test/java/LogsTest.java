54863d4dfc79bd12a3736665435f2883385771ce
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-26 13:24:58 +0700

    Написание теста для проверки ответа в логах

--- src/test/java/LogsTest.java
+++ src/test/java/LogsTest.java
@@ -1,8 +1,11 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import org.awaitility.Awaitility;
 import org.awaitility.core.ConditionFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogEntries;
@@ -30,6 +33,7 @@ public class LogsTest {
 
         WAITER.get().until(() -> {
             LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+            printLogs(logEntries);
             isLogContains.set(logEntries.getAll().stream().anyMatch(x -> x.getMessage().contains(expectedMessage)));
             return isLogContains.get();
         });
@@ -40,6 +44,13 @@ public class LogsTest {
         for(LogEntry entry:logEntries){
             System.out.println(entry);
         }
+    }
+
+    @Test
+    public void checkLogsWithDelayTest(){
+        Selenide.open("http://85.192.34.140/logdelay/");
+        boolean isLogExist = waitLogs("ThreadQA secret message after 5 sec");
+        Assertions.assertTrue(isLogExist,"Лог не пришёл");
     }
 
     @BeforeEach
