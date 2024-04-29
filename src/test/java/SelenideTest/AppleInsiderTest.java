0bb6d5b72d5ea0dddf9295a1811b031386cdd7d6
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-29 17:42:33 +0700

    Константы

--- src/test/java/SelenideTest/AppleInsiderTest.java
+++ src/test/java/SelenideTest/AppleInsiderTest.java
@@ -7,4 +7,7 @@ public class AppleInsiderTest extends BaseTest{
     //3. Ввести текст "Чем iPhone 13 отличается от iPhone 12" и нажать Enter.
     //4. В найденных статьях найти href атрибут первой найденной статьи
     //5. Убедиться, что href ссылка содержит слово iphone-13
+
+    private static final String BASE_URL = "https://appleinsider.ru/";
+    private static final String SEARCH_STRING = "Чем iPhone 13 отличается от iPhone 12";
 }
