648421a20302d0f83e6142b6266236b46e607c38
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-29 17:29:19 +0700

    Создаём метод настройки и инициализации браузера

--- src/test/java/SelenideTest/BaseTest.java
+++ src/test/java/SelenideTest/BaseTest.java
@@ -1,5 +1,17 @@
 package SelenideTest;
 
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public abstract class BaseTest {
 
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();    //Скачиваем хром драйвер и устанавливаем его
+        Configuration.browser = "chrome";           //Указываем браузер
+        Configuration.webdriverLogsEnabled = true;  //Указываем на работу с WebDriverManager
+        Configuration.browserSize = "1920x1080";    //Размер окна
+        Configuration.headless = true;              //Видимость окна браузера при выполнении теста
+
+
+    }
 }
