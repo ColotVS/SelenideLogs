5bdd152f2c7aac859616d1417b3edcf72d7ee919
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-29 17:39:12 +0700

    Условия теста

---
+++ src/test/java/SelenideTest/AppleInsiderTest.java
@@ -0,0 +1,10 @@
+package SelenideTest;
+
+public class AppleInsiderTest extends BaseTest{
+    //Задача:
+    //1. Открыть сайт https://appleinsider.ru/
+    //2. Нажать на кнопку поиска сверху справа.
+    //3. Ввести текст "Чем iPhone 13 отличается от iPhone 12" и нажать Enter.
+    //4. В найденных статьях найти href атрибут первой найденной статьи
+    //5. Убедиться, что href ссылка содержит слово iphone-13
+}
