e556d1412a8e23952d6e26dfe8d60e3084652f18
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-30 18:00:32 +0700

    Page Object, создаём класс для главной страницы

--- src/test/java/SelenideTest/MainPage.java
+++ src/test/java/SelenideTest/MainPage.java
@@ -1,7 +1,14 @@
 package SelenideTest;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
 public class MainPage {
     //Главная страница сайта appleinsider.ru
+    private final SelenideElement searchButton = $x("//form");
 
-
+    public void clickOnSearch (){
+        searchButton.click();
+    }
 }
