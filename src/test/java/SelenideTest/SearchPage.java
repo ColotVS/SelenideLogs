bfbf9ccebe6483b8dc1bee1a4637cedfa2213b52
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-30 18:29:15 +0700

    Метод возвращающий href первой статьи

--- src/test/java/SelenideTest/SearchPage.java
+++ src/test/java/SelenideTest/SearchPage.java
@@ -5,6 +5,13 @@ import com.codeborne.selenide.ElementsCollection;
 import static com.codeborne.selenide.Selenide.$$x;
 
 public class SearchPage {
-
     private final ElementsCollection articleTitle = $$x("//h2//a");
+
+    /**
+     * Возвращает href из первой статьи
+     * @return
+     */
+    public String getHrefOnFirstArticle(){
+        return articleTitle.first().getAttribute("href");
+    }
 }
