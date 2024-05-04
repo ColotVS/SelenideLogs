7a7e77adc311a0c7bb28c6606c8e29b3c9f00b0b
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-04 12:41:51 +0700

    Описание условий теста

--- src/test/java/SelenideTest/HeadHunterTest/HeadHunterTest.java
+++ src/test/java/SelenideTest/HeadHunterTest/HeadHunterTest.java
@@ -3,4 +3,14 @@ package SelenideTest.HeadHunterTest;
 import Core.BaseTest;
 
 public class HeadHunterTest extends BaseTest {
+    /**
+     * 1) Отрыть резюме, которое доступно по ссылке
+     * https://barnaul.hh.ru/resume/d07921a0ff0d18d7470039ed1f7a6f79517931?hhtmFrom=resume_list
+
+     * 2) Получить информацию о профиле используя HashMap или Class, в котором должны быть следующие атрибуты:
+     * String sex, int age, String city, boolean confirmedPhoneNumber, boolean readyToRelocate;
+
+     *  3) Убедиться в соответствии фактического и ожидаемого результата
+     *  Ожидаемый результат sex = "M", age = 25, city = "Новоалтайск", confirmedPhoneNumber = true, readyToRelocate = true.
+     */
 }
