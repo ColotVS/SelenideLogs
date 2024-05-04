8652a124460030e347e138caa99bcd1e2d80b3c2
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-04 13:22:39 +0700

    Первый способ получения "М" из "Мужчина", простой

--- src/test/java/SelenideTest/HeadHunterTest/PageObject/HeadHunterResumePage.java
+++ src/test/java/SelenideTest/HeadHunterTest/PageObject/HeadHunterResumePage.java
@@ -1,17 +1,30 @@
 package SelenideTest.HeadHunterTest.PageObject;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
 
 public class HeadHunterResumePage {
     /*
       Страница резюме с сайта Head Hunter
      */
+    private final SelenideElement gender = $x("//span[@data-qa='resume-personal-gender']");
 
     /**
      * Конструктор класса HeadHunterResumePage
+     *
      * @param url передаем ссылку которую необходимо открыть
      */
     public HeadHunterResumePage(String url) {
         Selenide.open(url);
     }
+
+    public String getGender() {
+        String genderValue = gender.getText();
+        if (genderValue.equals("Мужчина")) {
+            return "М";
+        } else return "Ж";
+    }
+
 }
