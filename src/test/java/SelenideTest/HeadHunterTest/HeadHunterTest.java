717c226faa51401ce88ed524f72c950d5d3363b0
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-04 15:37:44 +0700

    Создание ожидаемой HashMap с парами ключ-значение

--- src/test/java/SelenideTest/HeadHunterTest/HeadHunterTest.java
+++ src/test/java/SelenideTest/HeadHunterTest/HeadHunterTest.java
@@ -4,6 +4,10 @@ import Core.BaseTest;
 import SelenideTest.HeadHunterTest.PageObject.HeadHunterResumePage;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class HeadHunterTest extends BaseTest {
     /**
      * 1) Отрыть резюме, которое доступно по ссылке
@@ -19,7 +23,15 @@ public class HeadHunterTest extends BaseTest {
     private static final String BASE_URL = "https://barnaul.hh.ru/resume/c9b1fc850005ea401f0039ed1f6b50555a5769?query=QA+engineer&searchRid=1714802890239c1c903512a7e27ddcf0&hhtmFrom=resume_search_result";
 
     @Test
-    public void checkAttributesHashMap(){
+    public void checkAttributesHashMap() {
         HeadHunterResumePage headHunterResumePage = new HeadHunterResumePage(BASE_URL);
+        Map<String, Object> expectedAttributes = new HashMap<>() {{
+            put(HeadHunterResumePage.GENDER, "М");
+            put(HeadHunterResumePage.AGE, 31);
+            put(HeadHunterResumePage.CITY, "Барнаул");
+            put(HeadHunterResumePage.READY_TO_RELOCATE, false);
+            put(HeadHunterResumePage.READY_TO_BUSINESS_TRIPS, false);
+            put(HeadHunterResumePage.WORK_PERMIT, true);
+        }};
     }
 }
